acfdef3793ad02430acbd1fee604a58565717ef5
AlekandarAleksicEngIt <aleksandar.aleksic@eng.it>
2021-08-31 10:19:46 +0200

    initial commit

---
+++ WeatherApp/WeatherApp/ViewController.swift
@@ -0,0 +1,19 @@
+//
+//  ViewController.swift
+//  WeatherApp
+//
+//  Created by aleksandar.aleksic on 31.8.21..
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
